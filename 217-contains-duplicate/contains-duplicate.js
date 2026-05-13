39c744d3f810ac2b08d9b54a6dcedea17ca0360a
Rahul Saini <asmrahul17@gmail.com>
2026-05-14 00:59:38 +0530

    Time: 21 ms (36.43%) | Memory: 73.6 MB (71.31%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,37 @@
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+     
+     
+const containsDuplicate=(nums)=>{
+//         for (let i = 0; i < nums.length; i++) {
+//             for (let j = i + 1; j < nums.length; j++) {
+//                 if (nums[i] === nums[j]) {
+//                     return true;
+//                 }
+//             }
+//         }
+
+//sorting approach
+//     const sorted=nums.sort((a,b)=>a-b)
+//     for(let i=0;i<sorted.length;i++){
+//         if(sorted[i]===sorted[i+1]){
+//             return true;
+//         }
+//     }
+
+//hashset approach
+        // const seen=new Set()
+        // for(let i=0;i<nums.length;i++){
+        //     if(seen.has(nums[i])){
+        //         return true;
+        //     }
+        //     seen.add(nums[i])
+        // }
+
+//hashset length approach
+
+    return new Set(nums).size<nums.length
+
+}
