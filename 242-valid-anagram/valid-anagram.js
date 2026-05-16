eb1ef8f841a75de84f4f19d0092bb5fdbe999b51
Rahul Saini <asmrahul17@gmail.com>
2026-05-16 14:46:14 +0530

    Time: 17 ms (39.66%) | Memory: 56 MB (68.30%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,7 +4,44 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-        const ss=s.split("").sort().join("")
-        const st=t.split("").sort().join("")
-        return ss===st?true:false;
-};
+//         const ss=s.split("").sort().join("")
+//         const st=t.split("").sort().join("")
+//         return ss===st?true:false;
+    if(s.length!==t.length){
+        return false;
+    }
+    const frequencyMapS= new Map();
+    const frequencyMapT= new Map();
+    
+    for(let i=0;i< s.length; i++){
+        if(frequencyMapS.has(s[i])){
+            let frequency=frequencyMapS.get(s[i])
+            frequencyMapS.set(s[i],frequency+1)
+        }else{
+        frequencyMapS.set(s[i],1)      
+            
+        }
+        
+        if(frequencyMapT.has(t[i])){
+            let frequency=frequencyMapT.get(t[i])
+            frequencyMapT.set(t[i],frequency+1)
+        }else{
+        frequencyMapT.set(t[i],1)      
+        }
+    }
+
+    if(frequencyMapS.size!==frequencyMapT.size){
+        return false;
+    }
+  let flag=0
+  for(const [key,value] of frequencyMapS){
+    if(frequencyMapS.get(key)===frequencyMapT.get(key)){
+        flag=1;
+    }else{
+        return false;
+    }
+  }
+
+return flag===0?false:true;    
+
+ };
