cb901d40fe817638b327772d7a706a00e5654ea9
Rahul Saini <asmrahul17@gmail.com>
2026-05-17 15:39:49 +0530

    Time: 23 ms (90.42%) | Memory: 66.5 MB (84.17%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -2,15 +2,33 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
+var groupAnagrams = function (strs) {
     //SORTING THE ELEMENTS OF ARRAY AND STORING INTO HASHMAP APPROACH
-    let map= new Map()
-    for(let s of strs){
-        let key= s.split("").sort().join("")
-        if(!map.get(key)){
+    // let map= new Map()
+    // for(let s of strs){
+    //     let key= s.split("").sort().join("")
+    //     if(!map.get(key)){
+    //         map.set(key,[])
+    //     }
+    //     map.get(key).push(s)
+    // }
+    //    return [...map.values()]
+
+    //MAKING CHARACTER FREQUENCY ARRAY AS KEY AND STORING THE ACTUAL ELEMENTS AS THEIR VALUE IN THE FREQUENCY MAP
+
+    let map = new Map();
+    let count = new Array(26);
+    for (let s of strs) {
+        count.fill(0)
+        for(ch of s){
+            const index= ch.charCodeAt(0)-"a".charCodeAt(0)
+            count[index]++
+        }
+        const key= count.join("#")
+        if(!map.has(key)){
             map.set(key,[])
         }
         map.get(key).push(s)
     }
-       return [...map.values()]
+    return [...map.values()]
 };
