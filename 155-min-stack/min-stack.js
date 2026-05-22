d613f37f379cce1967360ea2b1c380badb864247
Rahul Saini <asmrahul17@gmail.com>
2026-05-22 18:06:55 +0530

    Time: 7 ms (91.13%) | Memory: 66.4 MB (15.44%) - LeetSync

---
+++ 155-min-stack/min-stack.js
@@ -0,0 +1,46 @@
+var MinStack = function () {
+  this.stack = [];
+  this.minStack = [];
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  this.stack.push(val);
+  if (this.minStack.length === 0) {
+    this.minStack.push(val);
+  } else {
+    let currentMin = this.minStack[this.minStack.length - 1];
+    if (val < currentMin) {
+      this.minStack.push(val);
+    } else {
+      this.minStack.push(currentMin);
+    }
+  }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  this.minStack.pop();
+  return this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.minStack[this.minStack.length - 1];
+};
+
+
