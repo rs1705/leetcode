0426ad097fa139997bf7a23546fd4691be9d5903
Rahul Saini <asmrahul17@gmail.com>
2026-06-08 21:36:27 +0530

    Time: 3 ms (86.83%) | Memory: 60.1 MB (32.18%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = (s) => {
+  function isAlphaNumeric(char) {
+    return /[a-z0-9]/i.test(char);
+  }
+
+  let l = 0,
+    r = s.length - 1;
+  while (l < r) {
+    if (!isAlphaNumeric(s[l])) {
+      l++;
+      continue;
+    }
+    if (!isAlphaNumeric(s[r])) {
+      r--;
+      continue;
+    }
+
+    if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+      return false;
+    } else {
+      l++;
+      r--;
+    }
+  }
+  return true;
+};
