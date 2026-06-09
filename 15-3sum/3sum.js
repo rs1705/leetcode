69ae45e3c910ff1f433a353c2eb62f0e5d7e7051
Rahul Saini <asmrahul17@gmail.com>
2026-06-09 17:33:28 +0530

    Time: 83 ms (12.44%) | Memory: 72.6 MB (47.63%) - LeetSync

---
+++ 15-3sum/3sum.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+  nums.sort((a, b) => a - b);
+  let result = [];
+  let left, right;
+  for (let i = 0; i < nums.length; i++) {
+    left = i + 1;
+    right = nums.length - 1;
+
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+      if (sum < 0) {
+        left++;
+      } else if (sum > 0) {
+        right--;
+      } else {
+        result.push([nums[i], nums[left], nums[right]]);
+        left++;
+        right--;
+        while (left < right && nums[left] === nums[left - 1]) left++;
+      }
+    }
+  }
+  console.log(result);
+  return result;
+};
