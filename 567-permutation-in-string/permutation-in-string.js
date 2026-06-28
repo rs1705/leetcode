dc68f3a91f991be0822aa6055e3737bb769efba9
Rahul Saini <asmrahul17@gmail.com>
2026-06-29 01:16:48 +0530

    Time: 72 ms (17.30%) | Memory: 62.2 MB (24.86%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.js
@@ -0,0 +1,39 @@
+var checkInclusion = function (s1, s2) {
+  const compare = (obj1, obj2) => {
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+      return false;
+    }
+    for (const key in obj1) {
+      if (obj1[key] !== obj2[key]) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  let left = 0;
+  let s1Count = {};
+  for (let s of s1) {
+    if (!s1Count[s]) {
+      s1Count[s] = 0;
+    }
+    s1Count[s] = s1Count[s] + 1;
+  }
+
+  let windowCount = {};
+  for (let right = 0; right < s2.length; right++) {
+    const ch = s2[right];
+    windowCount[ch] = (windowCount[ch] || 0) + 1;
+    if (right - left + 1 > s1.length) {
+      let leftCh = s2[left];
+      windowCount[leftCh]--;
+      if (windowCount[leftCh] === 0) delete windowCount[leftCh];
+      left++;
+    }
+
+    if (right - left + 1 === s1.length) {
+      if (compare(s1Count, windowCount)) return true;
+    }
+  }
+  return false;
+};
