60f4778d7296ddab4ff436248f6244d614788056
Rahul Saini <asmrahul17@gmail.com>
2026-07-02 22:40:47 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (27.19%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -1,17 +1,19 @@
 var searchMatrix = function (matrix, target) {
   for (let i = 0; i < matrix.length; i++) {
-      let start = 0;
-      let end = matrix[i].length - 1;
-      while (start <= end) {
-        let mid = Math.floor(start + (end - start) / 2);
-        if (target > matrix[i][mid]) {
-          start = mid + 1;
-        } else if (target < matrix[i][mid]) {
-          end = mid - 1;
-        } else {
-          return true;
-        }
-      
+    if (target < matrix[i][0] || target > matrix[i][matrix[i].length - 1]) {
+      continue;
+    }
+    let start = 0;
+    let end = matrix[i].length - 1;
+    while (start <= end) {
+      let mid = Math.floor(start + (end - start) / 2);
+      if (target > matrix[i][mid]) {
+        start = mid + 1;
+      } else if (target < matrix[i][mid]) {
+        end = mid - 1;
+      } else {
+        return true;
+      }
     }
   }
   return false;
